f5768b9711046b7cf024353593ba6c33c568a4ec
OmarVazquezLopez <omar.vazquez.lopez@hotmail.com>
2019-01-22 15:50:35 -0600

    1.0.0

--- IndiceDeMasaCorporal/IndiceDeMasaCorporal/Form1.Designer.cs
+++ IndiceDeMasaCorporal/IndiceDeMasaCorporal/Form1.Designer.cs
@@ -28,154 +28,198 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
 			this.label1 = new System.Windows.Forms.Label();
 			this.label2 = new System.Windows.Forms.Label();
 			this.label3 = new System.Windows.Forms.Label();
 			this.label4 = new System.Windows.Forms.Label();
 			this.label5 = new System.Windows.Forms.Label();
-			this.textBox1 = new System.Windows.Forms.TextBox();
-			this.textBox2 = new System.Windows.Forms.TextBox();
-			this.textBox3 = new System.Windows.Forms.TextBox();
-			this.textBox4 = new System.Windows.Forms.TextBox();
-			this.radioButton1 = new System.Windows.Forms.RadioButton();
-			this.radioButton2 = new System.Windows.Forms.RadioButton();
-			this.button1 = new System.Windows.Forms.Button();
-			this.button2 = new System.Windows.Forms.Button();
+			this.txtNombre = new System.Windows.Forms.TextBox();
+			this.txtEdad = new System.Windows.Forms.TextBox();
+			this.txtEstatura = new System.Windows.Forms.TextBox();
+			this.txtPeso = new System.Windows.Forms.TextBox();
+			this.rbMasculino = new System.Windows.Forms.RadioButton();
+			this.rbFemenino = new System.Windows.Forms.RadioButton();
+			this.btnCalcular = new System.Windows.Forms.Button();
+			this.btnGuardar = new System.Windows.Forms.Button();
+			this.pictureBox1 = new System.Windows.Forms.PictureBox();
+			this.label6 = new System.Windows.Forms.Label();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
 			this.SuspendLayout();
 			// 
 			// label1
 			// 
 			this.label1.AutoSize = true;
+			this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label1.ForeColor = System.Drawing.Color.DarkGoldenrod;
 			this.label1.Location = new System.Drawing.Point(12, 36);
 			this.label1.Name = "label1";
-			this.label1.Size = new System.Drawing.Size(50, 13);
+			this.label1.Size = new System.Drawing.Size(113, 13);
 			this.label1.TabIndex = 0;
-			this.label1.Text = "Nombre: ";
+			this.label1.Text = "Nombre completo: ";
 			// 
 			// label2
 			// 
 			this.label2.AutoSize = true;
+			this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label2.ForeColor = System.Drawing.Color.DarkGoldenrod;
 			this.label2.Location = new System.Drawing.Point(12, 88);
 			this.label2.Name = "label2";
-			this.label2.Size = new System.Drawing.Size(38, 13);
+			this.label2.Size = new System.Drawing.Size(44, 13);
 			this.label2.TabIndex = 1;
 			this.label2.Text = "Edad: ";
 			// 
 			// label3
 			// 
 			this.label3.AutoSize = true;
+			this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label3.ForeColor = System.Drawing.Color.DarkGoldenrod;
 			this.label3.Location = new System.Drawing.Point(12, 147);
 			this.label3.Name = "label3";
-			this.label3.Size = new System.Drawing.Size(37, 13);
+			this.label3.Size = new System.Drawing.Size(43, 13);
 			this.label3.TabIndex = 2;
 			this.label3.Text = "Sexo: ";
 			// 
 			// label4
 			// 
 			this.label4.AutoSize = true;
+			this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label4.ForeColor = System.Drawing.Color.DarkGoldenrod;
 			this.label4.Location = new System.Drawing.Point(12, 209);
 			this.label4.Name = "label4";
-			this.label4.Size = new System.Drawing.Size(72, 13);
+			this.label4.Size = new System.Drawing.Size(86, 13);
 			this.label4.TabIndex = 3;
 			this.label4.Text = "Estatura(cm): ";
 			// 
 			// label5
 			// 
 			this.label5.AutoSize = true;
+			this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label5.ForeColor = System.Drawing.Color.DarkGoldenrod;
 			this.label5.Location = new System.Drawing.Point(12, 267);
 			this.label5.Name = "label5";
-			this.label5.Size = new System.Drawing.Size(55, 13);
+			this.label5.Size = new System.Drawing.Size(65, 13);
 			this.label5.TabIndex = 4;
 			this.label5.Text = "Peso(kg): ";
 			// 
-			// textBox1
+			// txtNombre
 			// 
-			this.textBox1.Location = new System.Drawing.Point(90, 206);
-			this.textBox1.Name = "textBox1";
-			this.textBox1.Size = new System.Drawing.Size(148, 20);
-			this.textBox1.TabIndex = 5;
+			this.txtNombre.Location = new System.Drawing.Point(131, 33);
+			this.txtNombre.Name = "txtNombre";
+			this.txtNombre.Size = new System.Drawing.Size(148, 20);
+			this.txtNombre.TabIndex = 5;
 			// 
-			// textBox2
+			// txtEdad
 			// 
-			this.textBox2.Location = new System.Drawing.Point(90, 85);
-			this.textBox2.Name = "textBox2";
-			this.textBox2.Size = new System.Drawing.Size(148, 20);
-			this.textBox2.TabIndex = 6;
+			this.txtEdad.Location = new System.Drawing.Point(131, 85);
+			this.txtEdad.Name = "txtEdad";
+			this.txtEdad.Size = new System.Drawing.Size(148, 20);
+			this.txtEdad.TabIndex = 6;
 			// 
-			// textBox3
+			// txtEstatura
 			// 
-			this.textBox3.Location = new System.Drawing.Point(90, 33);
-			this.textBox3.Name = "textBox3";
-			this.textBox3.Size = new System.Drawing.Size(148, 20);
-			this.textBox3.TabIndex = 7;
+			this.txtEstatura.Location = new System.Drawing.Point(131, 206);
+			this.txtEstatura.Name = "txtEstatura";
+			this.txtEstatura.Size = new System.Drawing.Size(148, 20);
+			this.txtEstatura.TabIndex = 7;
 			// 
-			// textBox4
+			// txtPeso
 			// 
-			this.textBox4.Location = new System.Drawing.Point(90, 264);
-			this.textBox4.Name = "textBox4";
-			this.textBox4.Size = new System.Drawing.Size(148, 20);
-			this.textBox4.TabIndex = 8;
+			this.txtPeso.Location = new System.Drawing.Point(131, 264);
+			this.txtPeso.Name = "txtPeso";
+			this.txtPeso.Size = new System.Drawing.Size(148, 20);
+			this.txtPeso.TabIndex = 8;
 			// 
-			// radioButton1
+			// rbMasculino
 			// 
-			this.radioButton1.AutoSize = true;
-			this.radioButton1.Location = new System.Drawing.Point(90, 145);
-			this.radioButton1.Name = "radioButton1";
-			this.radioButton1.Size = new System.Drawing.Size(73, 17);
-			this.radioButton1.TabIndex = 9;
-			this.radioButton1.TabStop = true;
-			this.radioButton1.Text = "Masculino";
-			this.radioButton1.UseVisualStyleBackColor = true;
+			this.rbMasculino.AutoSize = true;
+			this.rbMasculino.Location = new System.Drawing.Point(131, 145);
+			this.rbMasculino.Name = "rbMasculino";
+			this.rbMasculino.Size = new System.Drawing.Size(73, 17);
+			this.rbMasculino.TabIndex = 9;
+			this.rbMasculino.TabStop = true;
+			this.rbMasculino.Text = "Masculino";
+			this.rbMasculino.UseVisualStyleBackColor = true;
 			// 
-			// radioButton2
+			// rbFemenino
 			// 
-			this.radioButton2.AutoSize = true;
-			this.radioButton2.Location = new System.Drawing.Point(181, 145);
-			this.radioButton2.Name = "radioButton2";
-			this.radioButton2.Size = new System.Drawing.Size(71, 17);
-			this.radioButton2.TabIndex = 10;
-			this.radioButton2.TabStop = true;
-			this.radioButton2.Text = "Femenino";
-			this.radioButton2.UseVisualStyleBackColor = true;
+			this.rbFemenino.AutoSize = true;
+			this.rbFemenino.Location = new System.Drawing.Point(226, 145);
+			this.rbFemenino.Name = "rbFemenino";
+			this.rbFemenino.Size = new System.Drawing.Size(71, 17);
+			this.rbFemenino.TabIndex = 10;
+			this.rbFemenino.TabStop = true;
+			this.rbFemenino.Text = "Femenino";
+			this.rbFemenino.UseVisualStyleBackColor = true;
 			// 
-			// button1
+			// btnCalcular
 			// 
-			this.button1.Location = new System.Drawing.Point(216, 322);
-			this.button1.Name = "button1";
-			this.button1.Size = new System.Drawing.Size(94, 28);
-			this.button1.TabIndex = 11;
-			this.button1.Text = "Calcular";
-			this.button1.UseVisualStyleBackColor = true;
+			this.btnCalcular.Location = new System.Drawing.Point(159, 310);
+			this.btnCalcular.Name = "btnCalcular";
+			this.btnCalcular.Size = new System.Drawing.Size(94, 28);
+			this.btnCalcular.TabIndex = 11;
+			this.btnCalcular.Text = "Calcular";
+			this.btnCalcular.UseVisualStyleBackColor = true;
+			this.btnCalcular.Click += new System.EventHandler(this.btnCalcular_Click);
 			// 
-			// button2
+			// btnGuardar
 			// 
-			this.button2.Location = new System.Drawing.Point(361, 322);
-			this.button2.Name = "button2";
-			this.button2.Size = new System.Drawing.Size(94, 28);
-			this.button2.TabIndex = 12;
-			this.button2.Text = "Guardar";
-			this.button2.UseVisualStyleBackColor = true;
+			this.btnGuardar.Enabled = false;
+			this.btnGuardar.Location = new System.Drawing.Point(580, 310);
+			this.btnGuardar.Name = "btnGuardar";
+			this.btnGuardar.Size = new System.Drawing.Size(94, 28);
+			this.btnGuardar.TabIndex = 12;
+			this.btnGuardar.Text = "Guardar";
+			this.btnGuardar.UseVisualStyleBackColor = true;
+			// 
+			// pictureBox1
+			// 
+			this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+			this.pictureBox1.Location = new System.Drawing.Point(449, 85);
+			this.pictureBox1.Name = "pictureBox1";
+			this.pictureBox1.Size = new System.Drawing.Size(330, 199);
+			this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+			this.pictureBox1.TabIndex = 13;
+			this.pictureBox1.TabStop = false;
+			// 
+			// label6
+			// 
+			this.label6.AutoSize = true;
+			this.label6.Font = new System.Drawing.Font("Elephant", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label6.ForeColor = System.Drawing.Color.DarkGoldenrod;
+			this.label6.Location = new System.Drawing.Point(449, 35);
+			this.label6.Name = "label6";
+			this.label6.Size = new System.Drawing.Size(270, 25);
+			this.label6.TabIndex = 14;
+			this.label6.Text = "Indice de Masa Corporal";
 			// 
 			// Form1
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.BackColor = System.Drawing.Color.LightSalmon;
 			this.ClientSize = new System.Drawing.Size(800, 362);
-			this.Controls.Add(this.button2);
-			this.Controls.Add(this.button1);
-			this.Controls.Add(this.radioButton2);
-			this.Controls.Add(this.radioButton1);
-			this.Controls.Add(this.textBox4);
-			this.Controls.Add(this.textBox3);
-			this.Controls.Add(this.textBox2);
-			this.Controls.Add(this.textBox1);
+			this.Controls.Add(this.label6);
+			this.Controls.Add(this.pictureBox1);
+			this.Controls.Add(this.btnGuardar);
+			this.Controls.Add(this.btnCalcular);
+			this.Controls.Add(this.rbFemenino);
+			this.Controls.Add(this.rbMasculino);
+			this.Controls.Add(this.txtPeso);
+			this.Controls.Add(this.txtEstatura);
+			this.Controls.Add(this.txtEdad);
+			this.Controls.Add(this.txtNombre);
 			this.Controls.Add(this.label5);
 			this.Controls.Add(this.label4);
 			this.Controls.Add(this.label3);
 			this.Controls.Add(this.label2);
 			this.Controls.Add(this.label1);
+			this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+			this.MaximizeBox = false;
+			this.MinimizeBox = false;
 			this.Name = "Form1";
-			this.Text = "Form1";
+			this.Text = "Peso ideal / IMC";
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
 			this.ResumeLayout(false);
 			this.PerformLayout();
 
@@ -188,14 +232,16 @@
 		private System.Windows.Forms.Label label3;
 		private System.Windows.Forms.Label label4;
 		private System.Windows.Forms.Label label5;
-		private System.Windows.Forms.TextBox textBox1;
-		private System.Windows.Forms.TextBox textBox2;
-		private System.Windows.Forms.TextBox textBox3;
-		private System.Windows.Forms.TextBox textBox4;
-		private System.Windows.Forms.RadioButton radioButton1;
-		private System.Windows.Forms.RadioButton radioButton2;
-		private System.Windows.Forms.Button button1;
-		private System.Windows.Forms.Button button2;
+		private System.Windows.Forms.TextBox txtNombre;
+		private System.Windows.Forms.TextBox txtEdad;
+		private System.Windows.Forms.TextBox txtEstatura;
+		private System.Windows.Forms.TextBox txtPeso;
+		private System.Windows.Forms.RadioButton rbMasculino;
+		private System.Windows.Forms.RadioButton rbFemenino;
+		private System.Windows.Forms.Button btnCalcular;
+		private System.Windows.Forms.Button btnGuardar;
+		private System.Windows.Forms.PictureBox pictureBox1;
+		private System.Windows.Forms.Label label6;
 	}
 }
 
--- IndiceDeMasaCorporal/IndiceDeMasaCorporal/Form1.cs
+++ IndiceDeMasaCorporal/IndiceDeMasaCorporal/Form1.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -15,6 +16,59 @@ namespace IndiceDeMasaCorporal
 		public Form1()
 		{
 			InitializeComponent();
+		}
+
+		private void btnCalcular_Click(object sender, EventArgs e)
+		{
+			string nombre;
+			int edad;
+			string sexo;
+			//Validación de campos vacios
+			if(txtNombre.Text == "" || txtEdad.Text == "" || txtEstatura.Text == "" || txtPeso.Text == "")
+			{
+				MessageBox.Show("Hace falta llenar algun campo.");
+				return;
+			}
+			if(!(rbMasculino.Checked || rbFemenino.Checked))
+			{
+				MessageBox.Show("Hace falta seleccionar el sexo.");
+				return;
+			}
+			//Validación del campo de texto Nombre
+			Regex expresionRegularNombre = new Regex("[A-Za-z0-9]");
+
+			if (expresionRegularNombre.IsMatch(txtNombre.Text))
+			{
+				nombre = txtNombre.Text;
+			}
+			else
+			{
+				MessageBox.Show("Solo valores alfanuméricos");
+				return;
+			}
+			//Validación del campo de texto Edad
+			Regex expresionRegularEdad = new Regex("[0-9]");
+
+			if(expresionRegularEdad.IsMatch(txtEdad.Text))
+			{
+				edad = Int32.Parse(txtEdad.Text);
+			}
+			else
+			{
+				MessageBox.Show("Solo valores numéricos");
+				return;
+			}
+
+			//Validación del campo de sexo
+			if(rbMasculino.Checked)
+			{
+				sexo = "Hombre";
+			}
+			else if(rbFemenino.Checked)
+			{
+				sexo = "Mujer";
+			}
+
 		}
 	}
 }
